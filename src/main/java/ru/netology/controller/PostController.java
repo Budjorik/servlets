6f1672e47dc1b311a71b9628ae82a22886d4fca0
Сергей Караульных <karaulnih@gmail.com>
2022-07-19 20:16:47 +0500

    Create files

---
+++ src/main/java/ru/netology/controller/PostController.java
@@ -0,0 +1,61 @@
+package ru.netology.controller;
+
+import com.google.gson.Gson;
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import ru.netology.service.PostService;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Reader;
+
+public class PostController {
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        final var data = service.all();
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        // Deserialize request & serialize response
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        try {
+            final var data = service.getById(id);
+            response.getWriter().print(gson.toJson(data));
+        } catch (NotFoundException exception) {
+            response.getWriter().print(gson.toJson("Id not found!"));
+        }
+    }
+
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        final var post = gson.fromJson(body, Post.class);
+        try {
+            final var data = service.save(post);
+            response.getWriter().print(gson.toJson(data));
+        } catch (NotFoundException exception) {
+            response.getWriter().print(gson.toJson("Id not found!"));
+        }
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        // Deserialize request & serialize response
+        response.setContentType(APPLICATION_JSON);
+        final var gson = new Gson();
+        try {
+            service.removeById(id);
+            response.getWriter().print(gson.toJson("Deleted post with id: " + id + "."));
+        } catch (NotFoundException exception) {
+            response.getWriter().print(gson.toJson("Id not found!"));
+        }
+    }
+}
---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,44 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+// Stub
+public class PostRepository {
+  private long counter;
+  private ConcurrentHashMap<Long, Post> posts = new ConcurrentHashMap<>();
+
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(posts.get(id));
+  }
+
+  public Post save(Post post) throws NotFoundException {
+    if (post.getId() == 0) {
+      counter++;
+      post.setId(counter);
+      posts.put(counter, post);
+      return posts.get(counter);
+    } else if (posts.containsKey(post.getId())) {
+      posts.replace(post.getId(), post);
+      return posts.get(post.getId());
+    } else {
+      throw new NotFoundException("Id not found!");
+    }
+  }
+
+  public void removeById(long id) {
+    if (posts.containsKey(id)) {
+      posts.remove(id);
+    } else {
+      throw new NotFoundException("Id not found!");
+    }
+  }
+}
